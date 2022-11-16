868a4ef0a3e9aa169688a88af41023e6aedc7381
professinal12 <50769481+professinal12@users.noreply.github.com>
2022-11-16 21:11:53 +0700

    bai tap ngay 16/11

--- ss1_mo_dau/bai_tap/student_management.sql
+++ ss1_mo_dau/bai_tap/student_management.sql
@@ -1,20 +1,20 @@
 create database student_management;
 create table student(
-id int not null,
-`name` varchar(50),
-age int null,
-country varchar(50),
+id int auto_increment not null,
+`name` varchar(50) not null,
+age int default  0,
+country varchar(50)not null,
 primary key(id)
 );
 
 create table class(
-class_id int,
-class_name varchar(50)
+id int auto_increment primary key not null,
+name varchar(50)not null
 );
 
 create table teacher(
-id int,
-`name` varchar(50),
+id int auto_increment primary key not null,
+`name` varchar(50) not null,
 age int,
-country varchar(50)
+country varchar(50)not null
 );
